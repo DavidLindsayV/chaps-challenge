2f7e0f95a3cedefa12d4d8b580fee48a7ca55578
Your Name <youremail@yourdomain.com>
2022-09-19 15:05:50 +1200

    Added player movement hooks

---
+++ nz/ac/vuw/ecs/swen225/gp22/domain/Player.java
@@ -0,0 +1,19 @@
+package nz.ac.vuw.ecs.swen225.gp22.domain;
+
+public class Player {
+    public void moveUp() {
+        return;
+    }
+
+    public void moveLeft() {
+        return;
+    }
+
+    public void moveRight() {
+        return;
+    }
+
+    public void moveDown() {
+        return;
+    }
+}
