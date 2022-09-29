b12bcb2c30ad5be0459c8a7164aee01e90d8f111
DavidLindsayV <83639079+DavidLindsayV@users.noreply.github.com>
2022-09-29 14:28:45 +1300

    Update method name used

--- nz/ac/vuw/ecs/swen225/gp22/persistency/MockPersistency.java
+++ nz/ac/vuw/ecs/swen225/gp22/persistency/MockPersistency.java
@@ -1,32 +1,30 @@
 package nz.ac.vuw.ecs.swen225.gp22.persistency;
 
 import java.io.IOException;
-
-import org.dom4j.DocumentException;
-
 import nz.ac.vuw.ecs.swen225.gp22.domain.Direction;
 import nz.ac.vuw.ecs.swen225.gp22.domain.Domain;
 import nz.ac.vuw.ecs.swen225.gp22.recorder.MockRecorder;
 import nz.ac.vuw.ecs.swen225.gp22.recorder.Recorder;
+import org.dom4j.DocumentException;
 
 public class MockPersistency {
-    /**
-     * Runs a simulation of a game
-     */
-    public static void run() {
 
-        try {
-            Recorder.newLevel();
-            for (int i = 0; i < 10; i++) {
-                Recorder.tick(MockRecorder.randomEnum(Direction.class));
-            }
+  /**
+   * Runs a simulation of a game
+   */
+  public static void run() {
+    try {
+      Recorder.setUp();
+      for (int i = 0; i < 10; i++) {
+        Recorder.tick(MockRecorder.randomEnum(Direction.class));
+      }
 
-            Domain d = Parser.loadLevel("level2.xml");
-            Parser.saveLevel(d);
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (DocumentException e) {
-            e.printStackTrace();
-        }
+      Domain d = Parser.loadLevel("level2.xml");
+      Parser.saveLevel(d);
+    } catch (IOException e) {
+      e.printStackTrace();
+    } catch (DocumentException e) {
+      e.printStackTrace();
     }
+  }
 }
