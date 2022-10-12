8eebf74e7ccc77ca462feac11969987a8d48dff7
Georgia Barrand <GMBarrand@gmail.com>
2022-10-12 23:41:49 +1300

    Add tests

--- nz/ac/vuw/ecs/swen225/gp22/persistency/PersistencyTests.java
+++ nz/ac/vuw/ecs/swen225/gp22/persistency/PersistencyTests.java
@@ -18,38 +18,39 @@ import java.nio.file.Files;
 import org.dom4j.DocumentException;
 
 public class PersistencyTests {
-	String test1Layout = "|_|_|_|_|_|_|\n"
-			+ "|_|_|_|_|_|_|\n"
-			+ "|_|_|_|*|P|_|\n"
-			+ "|_|_|L|#|#|#|\n"
-			+ "|_|_|E|_|_|_|\n";
-	
-	String level1Layout = "|_|_|#|#|#|#|#|_|#|#|#|#|#|_|_|\n"
-			+ "|_|_|#|_|_|_|#|#|#|_|_|_|#|_|_|\n"
-			+ "|_|_|#|_|_|_|#|E|#|_|_|_|#|_|_|\n"
-			+ "|#|#|#|#|#|@|#|L|#|@|#|#|#|#|#|\n"
-			+ "|#|_|k|_|@|_|_|_|_|_|@|_|k|_|#|\n"
-			+ "|#|_|*|_|#|k|_|i|_|k|#|_|*|_|#|\n"
-			+ "|#|#|#|#|#|*|_|P|_|*|#|#|#|#|#|\n"
-			+ "|#|_|*|_|#|k|_|_|_|k|#|_|*|_|#|\n"
-			+ "|#|_|_|_|@|_|_|*|_|_|@|_|_|_|#|\n"
-			+ "|#|#|#|#|#|#|@|#|@|#|#|#|#|#|#|\n"
-			+ "|_|_|_|_|#|_|_|#|_|_|#|_|_|_|_|\n"
-			+ "|_|_|_|_|#|_|*|#|*|_|#|_|_|_|_|\n"
-			+ "|_|_|_|_|#|_|k|#|k|_|#|_|_|_|_|\n"
-			+ "|_|_|_|_|#|#|#|#|#|#|#|_|_|_|_|\n";
-	
-	String level2Layout = "|_|_|#|#|#|#|#|#|#|_|_|_|_|_|_|_|\n"
-			+ "|_|_|#|_|_|*|_|_|#|_|_|_|_|_|_|_|\n"
-			+ "|_|_|#|_|_|_|_|_|#|_|_|_|_|_|_|_|\n"
-			+ "|_|_|#|_|_|#|_|_|#|#|#|#|#|#|#|#|\n"
-			+ "|#|#|#|_|_|#|_|_|_|#|k|_|_|_|*|#|\n"
-			+ "|#|E|L|_|_|#|#|_|_|#|#|#|P|i|_|#|\n"
-			+ "|#|#|#|_|_|#|_|4|_|@|_|_|_|_|*|#|\n"
-			+ "|_|_|#|_|_|#|4|_|#|#|#|#|#|#|#|#|\n"
-			+ "|_|_|#|_|_|4|_|_|#|_|_|_|_|_|_|_|\n"
-			+ "|_|_|#|_|_|*|_|_|#|_|_|_|_|_|_|_|\n"
-			+ "|_|_|#|#|#|#|#|#|#|_|_|_|_|_|_|_|\n";
+    String test1Layout = "|_|_|_|_|_|_|\n"
+            + "|_|_|_|_|_|_|\n"
+            + "|_|_|_|*|P|_|\n"
+            + "|_|_|L|#|#|#|\n"
+            + "|_|_|E|_|_|_|\n";
+
+    String level1Layout = "|_|_|#|#|#|#|#|_|#|#|#|#|#|_|_|\n"
+            + "|_|_|#|_|_|_|#|#|#|_|_|_|#|_|_|\n"
+            + "|_|_|#|_|_|_|#|E|#|_|_|_|#|_|_|\n"
+            + "|#|#|#|#|#|@|#|L|#|@|#|#|#|#|#|\n"
+            + "|#|_|k|_|@|_|_|_|_|_|@|_|k|_|#|\n"
+            + "|#|_|*|_|#|k|_|i|_|k|#|_|*|_|#|\n"
+            + "|#|#|#|#|#|*|_|P|_|*|#|#|#|#|#|\n"
+            + "|#|_|*|_|#|k|_|_|_|k|#|_|*|_|#|\n"
+            + "|#|_|_|_|@|_|_|*|_|_|@|_|_|_|#|\n"
+            + "|#|#|#|#|#|#|@|#|@|#|#|#|#|#|#|\n"
+            + "|_|_|_|_|#|_|_|#|_|_|#|_|_|_|_|\n"
+            + "|_|_|_|_|#|_|*|#|*|_|#|_|_|_|_|\n"
+            + "|_|_|_|_|#|_|k|#|k|_|#|_|_|_|_|\n"
+            + "|_|_|_|_|#|#|#|#|#|#|#|_|_|_|_|\n";
+
+    String level2Layout = "|_|_|#|#|#|#|#|#|#|_|_|_|_|_|_|_|\n"
+            + "|_|_|#|_|_|*|_|_|#|_|_|_|_|_|_|_|\n"
+            + "|_|_|#|_|_|_|_|_|#|_|_|_|_|_|_|_|\n"
+            + "|_|_|#|_|_|#|_|_|#|#|#|#|#|#|#|#|\n"
+            + "|#|#|#|_|_|#|_|_|_|#|k|_|_|_|*|#|\n"
+            + "|#|E|L|_|_|#|#|_|_|#|#|#|P|i|_|#|\n"
+            + "|#|#|#|_|_|#|_|4|_|@|_|_|_|_|*|#|\n"
+            + "|_|_|#|_|_|#|4|_|#|#|#|#|#|#|#|#|\n"
+            + "|_|_|#|_|_|4|_|_|#|_|_|_|_|_|_|_|\n"
+            + "|_|_|#|_|_|*|_|_|#|_|_|_|_|_|_|_|\n"
+            + "|_|_|#|#|#|#|#|#|#|_|_|_|_|_|_|_|\n";
+
     @Test
     public void testLoadingLevel() {
         try {
@@ -60,10 +61,10 @@ public class PersistencyTests {
         }
 
     }
-    
+
     @Test
     public void testLoadingLevel1() {
-    	try {
+        try {
             Domain d = Parser.loadLevel("level1.xml");
             assertEquals(d.toString(), level1Layout);
         } catch (DocumentException e) {
@@ -71,10 +72,10 @@ public class PersistencyTests {
         }
 
     }
-    
+
     @Test
     public void testLoadingLevel2() {
-    	try {
+        try {
             Domain d = Parser.loadLevel("level2.xml");
             System.out.println(d.toString());
             assertEquals(d.toString(), level2Layout);
@@ -82,14 +83,74 @@ public class PersistencyTests {
             assert false : e.getMessage();
         }
     }
-    
+
     @Test
     public void testNoRowNumber() {
-    	try {
+        try {
             Domain d = Parser.loadLevel("tests/noRowNumber.xml");
             assertThrows(NullPointerException.class, () -> System.out.println("No row number not detected"));
         } catch (DocumentException e) {
             assert false : e.getMessage();
+        }
+    }
+
+    @Test
+    public void testNoKeyColourSpecified() {
+        try {
+            Domain d = Parser.loadLevel("tests/keyWithNoColour.xml");
+            fail("Failed to detect no colour specified");
+        } catch (DocumentException e) {
+            assert false : e.getMessage();
+        } catch (NullPointerException e) {
+            assert true;
+        }
+    }
+
+    @Test
+    public void testNoPlayerPosition() {
+        try {
+            Domain d = Parser.loadLevel("tests/noPlayer.xml");
+            fail("Failed to detect no player specified");
+        } catch (DocumentException e) {
+            assert false : e.getMessage();
+        } catch (IllegalStateException e) {
+            assert true;
+        }
+    }
+
+    @Test
+    public void testNoExitPosition() {
+        try {
+            Domain d = Parser.loadLevel("tests/noExit.xml");
+            fail("Failed to detect no exit specified");
+        } catch (DocumentException e) {
+            assert false : e.getMessage();
+        } catch (IllegalStateException e) {
+            assert true;
+        }
+    }
+
+    @Test
+    public void testNoDoorColourSpecified() {
+        try {
+            Domain d = Parser.loadLevel("tests/doorWithNoColour.xml");
+            fail("Failed to detect no colour specified");
+        } catch (DocumentException e) {
+            assert false : e.getMessage();
+        } catch (NullPointerException e) {
+            assert true;
+        }
+    }
+
+    @Test
+    public void testNoEnemyPathSpecified() {
+        try {
+            Domain d = Parser.loadLevel("tests/enemyWithNoPath.xml");
+            fail("Failed to detect no path specified");
+        } catch (DocumentException e) {
+            assert false : e.getMessage();
+        } catch (NullPointerException e) {
+            assert true;
         }
     }
 
