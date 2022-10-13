5bfbaaa7036a89c888e2858201aeb15a39bd609c
lindsadavi2 <davidtlindsay@gmail.com>
2022-10-13 22:38:39 +1300

    Moved code stuff to correct files

--- nz/ac/vuw/ecs/swen225/gp22/fuzz/ArtificialInput.java
+++ nz/ac/vuw/ecs/swen225/gp22/fuzz/ArtificialInput.java
@@ -12,26 +12,4 @@ interface ArtificialInput {
     public void play(Robot b);
 }
 
-/**
- * Class for getting random artificial inputs
- *
- * @author Maximus De Leon deleomaxi 300566351
- *
- */
-public class BasicInput {
-	// list of inputs only contain up, down, left and right
-public static List<ArtificialInput> inputTypes = List.of(
-        (b)->{b.keyPress(KeyEvent.VK_UP); b.keyRelease(KeyEvent.VK_UP);},
-        (b)->{b.keyPress(KeyEvent.VK_DOWN); b.keyRelease(KeyEvent.VK_DOWN);},
-        (b)->{b.keyPress(KeyEvent.VK_LEFT); b.keyRelease(KeyEvent.VK_LEFT);},
-        (b)->{b.keyPress(KeyEvent.VK_RIGHT); b.keyRelease(KeyEvent.VK_RIGHT);}
-    );
 
-    /**
-     * Returns a random artificialinput from the given list above
-     * @return
-     */
-    public static ArtificialInput getRandom(){
-        return inputTypes.get(new Random().nextInt(inputTypes.size()));
-    }
-}
